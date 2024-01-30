7310ad42fc25c97693b88a5faf4cba9d263e0eee
Lukas Potthast <privat@lukas-potthast.de>
2024-01-30 14:18:47 +0100

    Allow id changes

--- leptos-tiptap/src/tiptap_instance.rs
+++ leptos-tiptap/src/tiptap_instance.rs
@@ -65,8 +65,9 @@ pub enum TiptapInstanceMsg {
 #[component]
 pub fn TiptapInstance(
     /// The ID of for this tiptap instance. Must be UNIQUE across ALL instances. You might want to use a UUID if uniqueness is otherwise not enforceable.
+    /// This may be a signal to support SSR. If the client should regenerate this ID, the old instance is removed (if one existed) and a new instance is created.
     #[prop(into)]
-    id: String,
+    id: MaybeSignal<String>,
 
     /// Classes, optional.
     #[prop(optional, into)]
@@ -106,10 +107,17 @@ pub fn TiptapInstance(
 ) -> impl IntoView {
     let instance: NodeRef<leptos::html::Custom> = create_node_ref();
 
-    let id = store_value(id);
+    let id: Signal<String> = Signal::derive(move || {
+        id.get()
+    });
 
     // This create_effect is purely there to make this SSR compatible.
-    create_effect(move |_| {
+    create_effect(move |old_id: Option<String>| {
+        // Rerun this effect whenever the ID should change!
+        if let Some(old_id) = &old_id {
+            js_tiptap::destroy(old_id.clone());
+        }
+
         // This closure is passed on to the JS tiptap instance.
         // We expect this to be called whenever the INPUT in the editor changes.
         // We have to own this closure until the end of this components lifetime!
@@ -145,7 +153,7 @@ pub fn TiptapInstance(
                             on_content_change_closure.with_value(|on_change_closure| {
                                 on_selection_change_closure.with_value(|on_selection_closure| {
                                     js_tiptap::create(
-                                        id.get_value(),
+                                        id.get_untracked(),
                                         value.get_untracked(),
                                         !disabled.get_untracked(),
                                         on_change_closure,
@@ -163,9 +171,6 @@ pub fn TiptapInstance(
             None
         });
 
-        on_cleanup(move || {
-            js_tiptap::destroy(id.get_value());
-        });
 
         // Talking to the tiptap instance here may ultimately trigger a content change.
         // This, and some other actions, may trigger callbacks reaching back to us using the closures above.
@@ -180,56 +185,56 @@ pub fn TiptapInstance(
             match msg {
                 TiptapInstanceMsg::Noop => {}
                 TiptapInstanceMsg::H1 => {
-                    js_tiptap::toggle_heading(id.get_value(), TiptapHeadingLevel::H1);
+                    js_tiptap::toggle_heading(id.get_untracked(), TiptapHeadingLevel::H1);
                 }
                 TiptapInstanceMsg::H2 => {
-                    js_tiptap::toggle_heading(id.get_value(), TiptapHeadingLevel::H2);
+                    js_tiptap::toggle_heading(id.get_untracked(), TiptapHeadingLevel::H2);
                 }
                 TiptapInstanceMsg::H3 => {
-                    js_tiptap::toggle_heading(id.get_value(), TiptapHeadingLevel::H3);
+                    js_tiptap::toggle_heading(id.get_untracked(), TiptapHeadingLevel::H3);
                 }
                 TiptapInstanceMsg::H4 => {
-                    js_tiptap::toggle_heading(id.get_value(), TiptapHeadingLevel::H4);
+                    js_tiptap::toggle_heading(id.get_untracked(), TiptapHeadingLevel::H4);
                 }
                 TiptapInstanceMsg::H5 => {
-                    js_tiptap::toggle_heading(id.get_value(), TiptapHeadingLevel::H5);
+                    js_tiptap::toggle_heading(id.get_untracked(), TiptapHeadingLevel::H5);
                 }
                 TiptapInstanceMsg::H6 => {
-                    js_tiptap::toggle_heading(id.get_value(), TiptapHeadingLevel::H6);
+                    js_tiptap::toggle_heading(id.get_untracked(), TiptapHeadingLevel::H6);
                 }
                 TiptapInstanceMsg::Paragraph => {
-                    js_tiptap::set_paragraph(id.get_value());
+                    js_tiptap::set_paragraph(id.get_untracked());
                 }
                 TiptapInstanceMsg::Bold => {
-                    js_tiptap::toggle_bold(id.get_value());
+                    js_tiptap::toggle_bold(id.get_untracked());
                 }
                 TiptapInstanceMsg::Italic => {
-                    js_tiptap::toggle_italic(id.get_value());
+                    js_tiptap::toggle_italic(id.get_untracked());
                 }
                 TiptapInstanceMsg::Strike => {
-                    js_tiptap::toggle_strike(id.get_value());
+                    js_tiptap::toggle_strike(id.get_untracked());
                 }
                 TiptapInstanceMsg::Blockquote => {
-                    js_tiptap::toggle_blockquote(id.get_value());
+                    js_tiptap::toggle_blockquote(id.get_untracked());
                 }
                 TiptapInstanceMsg::Highlight => {
-                    js_tiptap::toggle_highlight(id.get_value());
+                    js_tiptap::toggle_highlight(id.get_untracked());
                 }
                 TiptapInstanceMsg::AlignLeft => {
-                    js_tiptap::set_text_align_left(id.get_value());
+                    js_tiptap::set_text_align_left(id.get_untracked());
                 }
                 TiptapInstanceMsg::AlignCenter => {
-                    js_tiptap::set_text_align_center(id.get_value());
+                    js_tiptap::set_text_align_center(id.get_untracked());
                 }
                 TiptapInstanceMsg::AlignRight => {
-                    js_tiptap::set_text_align_right(id.get_value());
+                    js_tiptap::set_text_align_right(id.get_untracked());
                 }
                 TiptapInstanceMsg::AlignJustify => {
-                    js_tiptap::set_text_align_justify(id.get_value());
+                    js_tiptap::set_text_align_justify(id.get_untracked());
                 }
                 TiptapInstanceMsg::SetImage(resource) => {
                     js_tiptap::set_image(
-                        id.get_value(),
+                        id.get_untracked(),
                         resource.url,
                         resource.alt,
                         resource.title,
@@ -245,14 +250,23 @@ pub fn TiptapInstance(
             if !initialized.get_untracked() {
                 return;
             }
-            js_tiptap::set_editable(id.get_value(), !disabled);
+            js_tiptap::set_editable(id.get_untracked(), !disabled);
+        });
+
+        id.get_untracked()
+    });
+
+    // This is not part of the previous create_effect, as on_cleanup "pushes" the closure and must only be called once!
+    create_effect(move |_| {
+        on_cleanup(move || {
+            js_tiptap::destroy(id.get());
         });
     });
 
     view! {
         <leptos-tiptap-instance
             node_ref=instance
-            id=id.get_value()
+            id=move || id.get()
             class=class
             style=style
             aria-disabled=move || disabled.get()
