dd83eae00ec9c2a56b65b95aa6bf136a59430cc0
Lukas Potthast <privat@lukas-potthast.de>
2024-12-28 22:53:07 +0100

    Fix clippy lints

--- leptos-tiptap/examples/demo-csr/build.rs
+++ leptos-tiptap/examples/demo-csr/build.rs
@@ -8,13 +8,13 @@ pub fn main() {
     std::fs::create_dir_all(js_dir.clone()).unwrap();
     println!("cargo:warning=js dir created");
 
-    std::fs::File::create(js_dir.join("tiptap-bundle.min.js"))
+    let _ = std::fs::File::create(js_dir.join("tiptap-bundle.min.js"))
         .unwrap()
         .write(leptos_tiptap_build::TIPTAP_BUNDLE_MIN_JS.as_bytes())
         .unwrap();
     println!("cargo:warning=tiptap-bundle.min.js written");
 
-    std::fs::File::create(js_dir.join("tiptap.js"))
+    let _ = std::fs::File::create(js_dir.join("tiptap.js"))
         .unwrap()
         .write(leptos_tiptap_build::TIPTAP_JS.as_bytes())
         .unwrap();
